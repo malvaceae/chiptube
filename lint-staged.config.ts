054ef85396600d72042fa69537406517d87cf380
malvaceae <malvaceae@hotmail.com>
2026-08-20 04:16:57 +0900

    change format order

--- lint-staged.config.ts
+++ lint-staged.config.ts
@@ -5,9 +5,7 @@ import type { Configuration } from 'lint-staged';
  * lint-stagedの設定
  */
 const config: Configuration = {
-  '*.{js,jsx,mjs,cjs,ts,tsx,mts,cts}': ['oxfmt', 'oxlint --fix'],
-  '*.{json,jsonc,json5,css,scss,less,pcss,postcss,graphql,gql,graphqls,toml,yml,yaml,html,htm,xhtml,vue,svelte,md,markdown,mdx,hbs,handlebars,mjml}':
-    'oxfmt --no-error-on-unmatched-pattern',
+  '*': ['oxlint --fix --no-error-on-unmatched-pattern', 'oxfmt --no-error-on-unmatched-pattern'],
 };
 
 export default config;
